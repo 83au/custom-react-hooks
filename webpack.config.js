d445869b540dc4fdc6a9369e6ead472d7ecd4dad
Beau Sherman <brsherman88@gmail.com>
2023-05-11 21:06:57 -0700

    Add initial config and code

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -6,6 +7,22 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'index.bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  }
-}
+    path: path.resolve(__dirname, 'dist'),
+  },
+
+  devServer: {
+    static: './dist',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ],
+  },
+
+  plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })],
+};
