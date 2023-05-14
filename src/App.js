f38445f5c845f43892e1320958434178dfb7120a
Beau Sherman <brsherman88@gmail.com>
2023-05-13 20:02:22 -0700

    Add first hook

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import useTypewriter from './hooks/useTypewriter/use-typewriter.js';
 
 function App() {
-  return <h1>App</h1>;
+  const languages = ['HTML.', 'CSS.', 'JavaScript.'];
+  const message = useTypewriter({ text: languages, direction: 'both', repeat: -1, speed: 100 });
+  return <h1>Hi! I like to code in {message}</h1>;
 }
 
 export default App;
