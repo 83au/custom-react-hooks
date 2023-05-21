8f71da97f550ede0fbe2563d058a39beb3253ad7
Beau Sherman <brsherman88@gmail.com>
2023-05-20 19:18:12 -0700

    Small refactor

--- src/index.js
+++ src/index.js
@@ -3,7 +3,5 @@ import { createRoot } from 'react-dom/client';
 import App from './App.js';
 
 document.body.innerHTML = '<div id="root"></div>';
-
 const root = createRoot(document.getElementById('root'));
-
 root.render(<App />);
