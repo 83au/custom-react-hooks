191a679758092868fb16ae48e96f2576e63247a8
Beau Sherman <brsherman88@gmail.com>
2023-05-21 18:46:27 -0700

    Minor adjustments

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import useTypewriter from './hooks/useTypewriter/use-typewriter.js';
 
 function App() {
   const languages = ['HTML.', 'CSS.', 'JavaScript.'];
-  const message = useTypewriter({ text: languages, direction: 'both', repeat: -1, speed: 100 });
+  const message = useTypewriter({
+    text: languages,
+    direction: 'both',
+    repeat: -1,
+    speed: 100,
+  });
   return <h1>Hi! I like to code in {message}</h1>;
 }
 
